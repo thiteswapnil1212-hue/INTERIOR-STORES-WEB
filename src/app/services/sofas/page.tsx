4a78f9521519e6514bf6a3de79ad902616bb5388
swapnil thite <thiteswapnil1212@gmail.com>
2026-08-18 09:34:29 +0530

    sofa page changed

--- src/app/services/sofas/page.tsx
+++ src/app/services/sofas/page.tsx
@@ -1,9 +1,15 @@
 import SofaHero from "../../../components/sofa/SofaHero";
+import SofaStyles from "../../../components/sofa/SofaStyles";
+import SeatingFeatures from "../../../components/sofa/SeatingFeatures";
+import IntegratedDesign from "../../../components/sofa/IntegratedDesign";
 
 export default function SofasPage() {
   return (
     <main>
       <SofaHero />
+      <SofaStyles />
+      <SeatingFeatures />
+      <IntegratedDesign />
     </main>
   );
 }
